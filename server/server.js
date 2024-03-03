e9eea5ecdbb4a8edafce2d22b4c063d8db983afa
Mashook-Mohammad_Meshkat <mashook.mohammad.meshkat@g.bracu.ac.bd>
2024-03-04 00:59:57 +0600

    second commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+app.use(express.json());
+const dbConfig = require("./config/dbConfig");
+const port = process.env.PORT || 5000;
+
+const usersRoute = require("./routes/usersRoute");
+
+// deployment config
+const path = require("path");
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(port, () => console.log(`Node JS server listening on port ${port}`));
